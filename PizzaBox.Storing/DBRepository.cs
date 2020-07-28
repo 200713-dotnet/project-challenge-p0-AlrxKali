595c8000388b6dda3f1bd308f9ed4e949f15cc48
alejandro alemany <accual1013@gmail.com>
2020-07-28 07:45:26 -0500

    new

--- PizzaBox.Storing/DBRepository.cs
+++ PizzaBox.Storing/DBRepository.cs
@@ -2,25 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using PizzaBox.Storing;
-using PizzaBox.Domain.Models;
+using domain = PizzaBox.Domain.Models;
 
-namespace PizzaBox.Storing.Repositories
+namespace PizzaBox.Storing
 {
   public class DBRepository
-  {
-    private PizzaBoxDbContext _db = new PizzaBoxDbContext();
+  { 
+    private PizzaBoxDBContext _db = new PizzaBoxDBContext();
 
-    public void Create(domain.Pizza pizza)
+    public void Create(Pizza pizza)
     {
       var newPizza = new Pizza();
 
-      newPizza.Crust = new Crust() { Name = pizza.Crust.Name };
-      newPizza.Size = new Size() { Name = pizza.Size.Name };
-      newPizza.Name = pizza.Name;
+      newPizza.Crust = new Crust() { Name = pizza.Crust };
+      newPizza.Size = new Size() { Name = pizza.Size};
       var orderDate = DateTime.UtcNow;
-      //newPizza.DateModified = DateTime.Now;
-      //newPizza.Active = false;
-      //newPizza.UserModified = Identity.Hash;
 
       _db.Pizza.Add(newPizza);
       _db.SaveChanges();
@@ -42,9 +38,5 @@ namespace PizzaBox.Storing.Repositories
 
       return domainPizzaList;
     }
-
-    public void Update() {}
-
-    public void Delete() {}
   }
 }
